182cc39aba51c71ebe1a4f3e881aa5de34c7aa8e
Mehedee-Hassan <razin.meha@gmail.com>
2014-10-22 13:05:22 +0600

    v1u1

---
+++ CircleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double radius = 4;
+
+           Console.Write("Circle's,radius : ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Circle aCircle = new Circle(radius);
+            
+            
+            double diameter = aCircle.GetDiameter();
+            double perimeter = aCircle.GetPerimeter();
+            double area = aCircle.GetArea();
+
+           Console.WriteLine("Circle's,\n Diameter ="+diameter
+                                +"\n Perimeter = " +perimeter
+                                + "\n Area = "+area) ;
+            Console.ReadKey();
+        }
+    }
+}
